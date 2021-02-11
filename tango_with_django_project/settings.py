9dd85c9797b2e0ef9d0e92e8046efda44ad029d1
Joseph Mousa <2453578m@student.gla.ac.uk>
2021-02-11 04:45:30 +0200

    ninth commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS' : { 'min_length' : 6}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -103,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+LOGIN_URL = 'rango:login'
 
 
 # Internationalization
